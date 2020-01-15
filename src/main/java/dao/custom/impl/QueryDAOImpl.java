d409dc0b96b1bc83227ac7deba7114868d2423bf
IDilantha <isurudilantha@gmail.com>
2020-01-15 22:01:26 +0530

    Complete QueryDao

--- src/main/java/dao/custom/impl/QueryDAOImpl.java
+++ src/main/java/dao/custom/impl/QueryDAOImpl.java
@@ -3,14 +3,11 @@ package dao.custom.impl;
 
 import dao.custom.QueryDAO;
 import entity.CustomEntity;
-import org.hibernate.Session;
 import org.hibernate.query.NativeQuery;
 import org.hibernate.query.Query;
 import org.hibernate.transform.Transformers;
 
 import javax.persistence.EntityManager;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 
 public class QueryDAOImpl implements QueryDAO {
@@ -19,10 +16,10 @@ public class QueryDAOImpl implements QueryDAO {
 
     @Override
     public List<CustomEntity> getOrdersInfo() throws Exception {
-        List resultList = entityManager.createNativeQuery("SELECT O.id as orderId, C.customerId as customerId, C.name as customerName, O.date as orderDate, SUM(OD.qty * OD.unitPrice) AS orderTotal  FROM Customer C INNER JOIN `Order` O ON C.customerId=O.customerId INNER JOIN  OrderDetail OD on O.id = OD.orderId GROUP BY O.id ").getResultList();
-       // Query<CustomEntity> query = nativeQuery.setResultTransformer(Transformers.aliasToBean(CustomEntity.class));
-        //List<CustomEntity> list = query.list();
-        return null;
+       NativeQuery nativeQuery = (NativeQuery)entityManager.createNativeQuery("SELECT O.id as orderId, C.customerId as customerId, C.name as customerName, O.date as orderDate, SUM(OD.qty * OD.unitPrice) AS orderTotal  FROM Customer C INNER JOIN `Order` O ON C.customerId=O.customerId INNER JOIN  OrderDetail OD on O.id = OD.orderId GROUP BY O.id ");
+       Query<CustomEntity> query = nativeQuery.setResultTransformer(Transformers.aliasToBean(CustomEntity.class));
+       List<CustomEntity> list = query.list();
+        return list;
     }
 
     @Override
